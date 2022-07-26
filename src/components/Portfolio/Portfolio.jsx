bed5e663f81d85bbebcfc9605205d6c9bded3778
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-07-26 23:26:46 +0300

    new one

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,6 +1,5 @@
 import './Portfolio.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import arrow from '../../images/mainarrow.svg'; 
 
 const Portfolio = () => {
@@ -8,24 +7,19 @@ const Portfolio = () => {
     return (
         <section className='portfolio'>
             <h8 className='portfolio__subtitle'>Портфолио</h8>
-            <Link to='https://yippeesmile.github.io/how-to-learn' className='portfolio__link'>
+            
                 <div className='portfolio__web'>
-                    <p className='portfolio__web_text'>Статичный сайт</p>
-                    <img src={arrow} alt='стрелка'></img>
+                    <p className='portfolio__web_text'><a href='https://yippeesmile.github.io/how-to-learn' target="_blank" rel="noopener noreferrer" className='portfolio__link'>Статичный сайт</a></p>
+                    <img src={arrow} alt='стрелка' className='portfolio__arrow-icon'></img>
                 </div>
-            </Link>
-            <Link to='https://yippeesmile.github.io/russian-travel/index.html' className='portfolio__link'>
                 <div className='portfolio__web'>
-                    <p className='portfolio__web_text'>Адаптивный сайт</p>
-                    <img src={arrow} alt='стрелка'></img>
+                    <p className='portfolio__web_text'><a href='https://yippeesmile.github.io/russian-travel/index.html' target="_blank" rel="noopener noreferrer" className='portfolio__link'>Адаптивный сайт</a></p>
+                    <img src={arrow} alt='стрелка' className='portfolio__arrow-icon'></img>
                 </div>
-            </Link>
-            <Link to='' className='portfolio__link'>
                 <div className='portfolio__web'>
                     <p className='portfolio__web_text'>Одностраничное приложение</p>
-                    <img src={arrow} alt='стрелка'></img>
+                    <img src={arrow} alt='стрелка' className='portfolio__arrow-icon'></img>
                 </div>
-            </Link>
         </section>
     )
 }
