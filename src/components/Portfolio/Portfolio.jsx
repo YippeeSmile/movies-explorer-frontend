b8671cc317214dd2a3eec3c59745732351f30fcc
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-07-28 00:28:42 +0300

    change tag

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -5,7 +5,7 @@ const Portfolio = () => {
 
     return (
         <section className='portfolio'>
-            <h7 className='portfolio__subtitle'>Портфолио</h7>
+            <h6 className='portfolio__subtitle'>Портфолио</h6>
                 <ul className='portfolio__web-list'>
                     <li className='portfolio__web'>
                         <a href='https://yippeesmile.github.io/how-to-learn' target="_blank" rel="noopener noreferrer" className='portfolio__link'>Статичный сайт</a>
