391309914e5301871462084e710d756f4e36892c
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-07-28 09:10:18 +0300

    fix 320

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -8,8 +8,10 @@ const SearchForm = () => {
     return (
         <section className='search-form'>
             <form className='search-form__container'>
+                <div className='search-form__container_left-column'>
                 <input type='text' className='search-form__input' placeholder='Фильм' minLength="2" required ></input>
                 <button type='submit' className='search-form__button'><img src={zoom} className='search-form__icon-button' alt='лупа' /></button>
+                </div>
                 <FilterSearch />
             </form>
         </section>
