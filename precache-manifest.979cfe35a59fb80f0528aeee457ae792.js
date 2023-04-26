2443bfad2e0fd124c170f332ddb071c988b926f6
Yuliya Padzheva <yippee.smile@yandex.ru>
2023-04-26 12:03:05 +0300

    Updates

---
+++ precache-manifest.979cfe35a59fb80f0528aeee457ae792.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3ffaedcd7218b34cb0b",
+    "url": "/movies-explorer-frontend/static/css/main.92b34539.chunk.css"
+  },
+  {
+    "revision": "d3ffaedcd7218b34cb0b",
+    "url": "/movies-explorer-frontend/static/js/main.d3ffaedc.chunk.js"
+  },
+  {
+    "revision": "c6b5a3e0f92ee25b3986",
+    "url": "/movies-explorer-frontend/static/js/1.c6b5a3e0.chunk.js"
+  },
+  {
+    "revision": "17334cd89d1b5c537e06",
+    "url": "/movies-explorer-frontend/static/js/2.17334cd8.chunk.js"
+  },
+  {
+    "revision": "bf8476192e922433722c",
+    "url": "/movies-explorer-frontend/static/js/runtime~main.bf847619.js"
+  },
+  {
+    "revision": "15e6ed05d18885ea3e7b64fb40ef87b5",
+    "url": "/movies-explorer-frontend/static/media/logo_main.15e6ed05.svg"
+  },
+  {
+    "revision": "46567c3e7158db5dbfe5b386d778704f",
+    "url": "/movies-explorer-frontend/static/media/movie.46567c3e.png"
+  },
+  {
+    "revision": "9986b8975eaddbfa933ab845f6053e45",
+    "url": "/movies-explorer-frontend/static/media/movie1.9986b897.png"
+  },
+  {
+    "revision": "4c4ac91588aed5256d08a2d6f3cd3519",
+    "url": "/movies-explorer-frontend/static/media/movie2.4c4ac915.png"
+  },
+  {
+    "revision": "6cfcb95405a5d4d236469085b9bd08e6",
+    "url": "/movies-explorer-frontend/static/media/movie3.6cfcb954.png"
+  },
+  {
+    "revision": "793bcbf63624fab26437cbf3a02dc0ef",
+    "url": "/movies-explorer-frontend/static/media/movie4.793bcbf6.png"
+  },
+  {
+    "revision": "14aa32cd76f79a5c040f8728f1878ad6",
+    "url": "/movies-explorer-frontend/static/media/burger__icon.14aa32cd.svg"
+  },
+  {
+    "revision": "15629f8b78d27b00afbd4633e109edd3",
+    "url": "/movies-explorer-frontend/static/media/exit.15629f8b.svg"
+  },
+  {
+    "revision": "0d70e8727e63f975def3499c24b12a17",
+    "url": "/movies-explorer-frontend/static/media/about-me_photo.0d70e872.png"
+  },
+  {
+    "revision": "acb3d991568bfc0ba5fd0356f5dc2e59",
+    "url": "/movies-explorer-frontend/static/media/zoom-icon-btn.acb3d991.svg"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/movies-explorer-frontend/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/movies-explorer-frontend/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/movies-explorer-frontend/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/movies-explorer-frontend/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "99a0d9a7e4c99c17bfdd94a22a5cf94e",
+    "url": "/movies-explorer-frontend/static/media/Inter-Bold.99a0d9a7.woff"
+  },
+  {
+    "revision": "444a7284663a3bc886683eb81450b294",
+    "url": "/movies-explorer-frontend/static/media/Inter-Bold.444a7284.woff2"
+  },
+  {
+    "revision": "2301690ea6a8fac49d9897b40ce8baa0",
+    "url": "/movies-explorer-frontend/static/media/icon__account.2301690e.svg"
+  },
+  {
+    "revision": "5cb8b6c5898cccd76aa4535188c9f18d",
+    "url": "/movies-explorer-frontend/static/media/mainarrow.5cb8b6c5.svg"
+  },
+  {
+    "revision": "2675efb888ea68150f780ee548abb7ae",
+    "url": "/movies-explorer-frontend/static/media/zoom-icon.2675efb8.svg"
+  },
+  {
+    "revision": "7bfef98769671d416b26af1037684f14",
+    "url": "/movies-explorer-frontend/index.html"
+  }
+];
